e3c2f1e909ce7915f758289e15ceeffca167fb8b
Michael <50381321+michalisKout@users.noreply.github.com>
2024-04-21 22:43:05 +0300

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
+OS=${OS:-macos}
+
 echo "Copy ZSH config ..."
 cp ./.zshenv ~/.zshenv
-cp ./.zshrc ~/.zshrc
+cp ./.zshrc-$OS ~/.zshrc
 
 echo "Source dot files ..."
 source ~/.zshenv

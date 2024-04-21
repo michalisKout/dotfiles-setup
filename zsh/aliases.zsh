8af5c90686680332a36a25057a8d6f2dab160776
Michael <50381321+michalisKout@users.noreply.github.com>
2024-04-21 17:55:19 +0300

    Create aliases.zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# yarn
+alias yd='yarn dev'
+alias ys='yarn start'
+
+# homebrew
+alias brewup='brew update; brew upgrade; brew cleanup; brew doctor'
+
+# npm
+alias npi='npm install'
+alias npu='npm uninstall'
+alias npd='npm run dev'
+alias nps='npm run start'
